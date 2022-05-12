c3046e185736cc9861015d5e8c1cb1f6bd8a6bad
thud aneknual <monhomes@hotmail.com>
2022-05-13 02:24:22 +0700

    Add People details page show people data
    Add DataAccess to connect database with Dapper
    
    Mod Layout

---
+++ RazorWebAppGitHub/Controllers/PeopleListController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RazorWebAppGitHub.DataAccess;
+using RazorWebAppGitHub.Models;
+
+namespace RazorWebAppGitHub.Controllers
+{
+    public class PeopleListController : Controller
+    {
+        DataConnection dataConnection = new DataConnection();
+        IEnumerable<People> peoples = new List<People>();
+        // GET: PeopleListController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PeopleList()
+        {
+            peoples = dataConnection.GetConnect();
+            ViewBag.PeopleGet = peoples;
+            return View();
+        }
+
+        // GET: PeopleListController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PeopleListController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PeopleListController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PeopleListController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PeopleListController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PeopleListController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PeopleListController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RazorWebAppGitHub/DataAccess/DataConnection.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using RazorWebAppGitHub.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RazorWebAppGitHub.DataAccess
+{
+    public class DataConnection
+    {
+        string connectionString = "Data Source=.;Initial Catalog=Sample;Integrated Security=True";
+        public IEnumerable<People> GetConnect()
+        {
+            using IDbConnection connection = new SqlConnection(connectionString);
+            IEnumerable<People> PeopleList = connection.Query<People>("dbo.People_GetAll", new {}, commandType: CommandType.StoredProcedure).ToList<People>();
+            return PeopleList;
+        }
+    }
+}
